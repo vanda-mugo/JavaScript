e033ab1f986ea3a7dc22901740893eda5358ac67
vanda-mugo <johnmugo006@gmail.com>
2025-01-10 23:35:29 +0300

    Added banner functionality to the portfolio page

--- PersonalPortfolio/vanda-portfolio/src/App.tsx
+++ PersonalPortfolio/vanda-portfolio/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { NavBar } from './components/NavBar/NavBar';
-
+import { Banner } from './components/Banner/Banner';
 function App() {
 
   return (
     <div className='App'>
       <NavBar />
+      <Banner />
     </div>
   )
 }
--- PersonalPortfolio/vanda-portfolio/src/components/NavBar/NavBar.tsx
+++ PersonalPortfolio/vanda-portfolio/src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ import navIcon2 from '../../assets/img/nav-icon2.svg';
 import navIcon3 from '../../assets/img/nav-icon3.svg';
 import './NavBar.css';
 
-export const NavBar = () => {
+export const NavBar = () : JSX.Element => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
