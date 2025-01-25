234628a61a7686f5dda52a769ed7b9642df46360
vanda-mugo <johnmugo006@gmail.com>
2025-01-25 13:14:41 +0300

    Added splash cursor and did some changes to the banner background

--- PersonalPortfolio/vanda-portfolio/src/App.tsx
+++ PersonalPortfolio/vanda-portfolio/src/App.tsx
@@ -3,11 +3,13 @@ import { NavBar } from './components/NavBar/NavBar';
 import { Banner } from './components/Banner/Banner';
 import { Skills } from './components/Skills/Skills';
 import { Services } from './components/Services/Services';
+import SplashCursor from  './components/SplashCursor/SplashCursor';
 
 function App() {
 
   return (
     <div className='App'>
+      < SplashCursor />
       <NavBar />
       <Banner />
       < Skills />
@@ -19,4 +21,4 @@ function App() {
 export default App
 
 
-
+//      < SplashCursor />
